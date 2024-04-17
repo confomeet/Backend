25d1c3229343126e8a17dcc0355781472a2ea45b
Alexey Minimulin <a.s.minimulin@gmail.com>
2024-04-17 09:26:04 +0300

    Removed unused DTOs

--- VideoProjectCore6/DTOs/CommonDto/UploadFileDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VideoProjectCore6.DTOs.CommonDto
-#nullable disable
-{
-    public class UploadFileDto
-    {
-        public IFormFile File { get; set; }
-        public string Target { get; set; }
-    }
-}
